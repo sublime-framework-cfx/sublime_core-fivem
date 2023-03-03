3d3e1a1332df83573b3f5d205d5fe377d0e15588
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-03-04 00:45:05 +0100

    #update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,19 @@
 fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
 game 'gta5'
 lua54 'yes'
-use_fxv2_oal 'yes'
-description 'Sublime Développement [sublime_core]'
-version '1.0.0'
 
+name 'sublime_core'
+author 'SUBLiME'
+version '0.0.0'
+repository 'https://github.com/SUBLiME-Association/sublime_core'
+description 'Un framework pour tout type de serveur'
 
+shared_script '_g.lua'
+shared_script 'obj.lua'
 
-
-shared_scripts {
-
+files {
+    'import.lua',
     'imports/**/shared.lua',
-    'imports/**/shared/*.lua',
-}
-
-
-server_scripts {
-    
-    'imports/**/server.lua',
-    'imports/**/server/*.lua',
-}
-
-client_scripts {
-    
     'imports/**/client.lua',
-    'imports/**/client/*.lua',
 }
