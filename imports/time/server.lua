d4d87bdb39438d437c22cb1408a6bfd0842cde46
NikXpro <briffanathan781@gmail.com>
2023-03-05 14:59:23 +0100

    update module time

--- imports/time/server.lua
+++ imports/time/server.lua
@@ -1,22 +1,26 @@
-
---- sl.time_setCurrent
-sl.time_getTime = function()
-    return (os.time())
+--- time.get_time: get the current time in seconds
+---@return number
+local function getTime()
+    return os.time()
 end
 
-
---- sl.time_getCurrent
-sl.time_getDate = function()
+--- time.get_date: get the current date as a string
+---@return string
+local function getDate()
     local actualTime = os.date("*t")
-    return (("%s/%s/%s %sh%s"):format(actualTime.day, actualTime.month, actualTime.year, actualTime.hour, actualTime.min))
+    return ("%s/%s/%s %sh%s"):format(actualTime.day, actualTime.month, actualTime.year, actualTime.hour, actualTime.min)
 end
 
-
---- sl.time_decode
----@param timeDecode any
-sl.time_decode = function(timeDecode)
-    local actualTime = os.date("*t", timeDecode)
-    return (("%s/%s/%s %sh%s"):format(actualTime.day, actualTime.month, actualTime.year, actualTime.hour, actualTime.min))
+--- time.decode: decode a time value and return it as a string
+---@param decodeTime any
+---@return string
+local function decodeTime(decodeTime)
+    local actualTime = os.date("*t", decodeTime)
+    return ("%s/%s/%s %sh%s"):format(actualTime.day, actualTime.month, actualTime.year, actualTime.hour, actualTime.min)
 end
 
-
+return {
+    get_time = getTime,
+    get_date = getDate,
+    decode = decodeTime,
+}
