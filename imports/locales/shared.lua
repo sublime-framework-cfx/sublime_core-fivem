e51b7831c3e0bade3dd6425db670cfee174487ed
SUP2Ak <cormier.wesley@gmail.com>
2023-05-07 16:23:47 +0200

    refactor: (imports.locales) up to date

--- imports/locales/shared.lua
+++ imports/locales/shared.lua
@@ -1,18 +1,96 @@
+local data = {} ---@type { [string]: string}
 
-local function LoadLocales(nation_prefix)
-    local lang <const> = GetConvar("sl_lang", nation_prefix or "fr")
-    local currentfile <const> = sl.string.split(sl.string.split(debug.getinfo(2, "Sl").source, "@")[3], "/" )
-    local content <const> = LoadResourceFile(currentfile[1], ("locales/lang_%s.lua"):format(lang))
-    if not content then
-        return error(("Could not load [Local file (%s)] or not exist !"):format(lang), 3)
+---@param str string
+---@param ... any
+---@return string
+function translate(str, ...)
+    local _t = data[str]
+    if _t then
+        if ... then
+            return _t:format(...)
+        end
+
+        return _t
     end
-    local func, err = load(content)
-    if not func or err then
-        return error(("Erreur pendant le chargement du module [LOCALE] (%s)"):format(err), 3)
+    return str
+end
+
+--- sl.locale.init
+local function Initialize()
+    local env = sl.json.load(('locales/%s'):format(sl.lang))
+    if not env then
+       return warn(("Impossible de chargé locales/%s.json dans l'environnement : %s"):format(sl.lang, sl.env))
     end
-    return func()
+
+    for k, v in pairs(env) do
+        if not data[k] then
+            if type(v) == 'string' then
+                for var in v:gmatch('${[%w%s%p]-}') do -- credit: ox_lib <https://github.com/overextended/ox_lib/blob/master/imports/locale/shared.lua>
+                    local locale = env[var:sub(3, -2)]
+
+                    if locale then
+                        locale = locale:gsub('%%', '%%%%')
+                        v = v:gsub(var, locale)
+                    end
+                end
+            end
+            data[k] = v
+        end
+    end
+end
+
+--- sl.locale.get
+---@param key? string
+---@return table|string
+local function GetInitialized(key)
+    return data[key] or data
+end
+
+--- sl.locale.load ( example : sl.locale.load('es_extended', 'locales.en', 'lua') | sl.locale.load('sl_core', 'locales.en', 'json') )
+---@param resource string
+---@param path string
+---@param ext string
+local function LoadExternal(resource, path, ext)
+    local filePath, folder, result = path, resource
+    filePath = filePath:gsub('%.', '/')
+
+    if ext == 'lua' then
+        local import = LoadResourceFile(folder, filePath..'.lua')
+        local func, err = load(import, ('@@%s/%s.lua'):format(folder, filePath))
+        if not func or err then
+            return error(err or ("unable to load module '%s/%s.lua'"):format(folder, filePath), 3)
+        end
+
+        result = func()
+    elseif ext == 'json' then
+        filePath = filePath:gsub('%.json', '')
+        result = sl.json.load(filePath, folder)
+    end
+
+    if type(result) ~= 'table' then
+        return error(("Le fichier '%s/%s' ne retourne pas une table mais (%s)"):format(folder, filePath, type(result)), 2)
+    end
+
+    for k, v in pairs(result) do
+        if not data[k] then
+            if type(v) == 'string' then
+                for var in v:gmatch('${[%w%s%p]-}') do -- credit: ox_lib <https://github.com/overextended/ox_lib/blob/master/imports/locale/shared.lua>
+                    local locale = result[var:sub(3, -2)]
+
+                    if locale then
+                        locale = locale:gsub('%%', '%%%%')
+                        v = v:gsub(var, locale)
+                    end
+                end
+            end
+
+            data[k] = v
+        end
+    end
 end
 
 return {
-    load = LoadLocales
+    init = Initialize,
+    get = GetInitialized,
+    load = LoadExternal,
 }
