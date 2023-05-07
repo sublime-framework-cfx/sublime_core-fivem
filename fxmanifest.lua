76ec69f28a86b21f896bffca5c0fe3bb403a3484
SUP2Ak <cormier.wesley@gmail.com>
2023-05-07 16:43:36 +0200

    chore; (fxmanifest) not finish yet

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,23 +9,27 @@ version '0.0.0'
 repository 'https://github.com/SUBLiME-Association/sublime_core'
 description 'Un framework pour tout type de serveur'
 
-shared_script '_g.lua'
-shared_script 'shared/*.lua'
+shared_script '_g.lua' -- need to be init first
 
-client_script 'client/**.lua'
+shared_scripts {
+    'config/shared/*.lua', -- on top
+}
 
-server_script 'server/**.lua'
+client_scripts {
+    'config/client/*.lua', -- on top
+}
 
-ui_page 'web/build/index.html'
+server_scripts {
+    'config/server/*.lua', -- on top
+}
+
+-- ui_page 'web/build/index.html'
 
 files {
     'obj.lua',
-    'locales/*.lua',
+    'locales/*',
     'imports/**/shared.lua',    
     'imports/**/client.lua',
-    'shared/data/*.json',
-    'web/build/index.html',
-    'web/build/**/*'
 }
 
 
