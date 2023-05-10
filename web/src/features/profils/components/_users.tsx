de91c09b916d1df669516600e7e0ad9acfc02b03
SUP2Ak <cormier.wesley@gmail.com>
2023-05-11 01:48:46 +0200

    refactor(web._users): MiniMenu closable click outside + add Change logo

--- web/src/features/profils/components/_users.tsx
+++ web/src/features/profils/components/_users.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronLeft, faPersonWalkingDashedLineArrowRight, faUserPen } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp, faChevronLeft, faPersonWalkingDashedLineArrowRight, faUserPen, faIdBadge } from '@fortawesome/free-solid-svg-icons';
 import { UnstyledButton, Group, Avatar, Text, Box, useMantineTheme, rem, Menu } from '@mantine/core';
 import style from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark';
 
@@ -16,7 +16,7 @@ export const User: React.FC<UserProps> = ({ username, permission, logo }) => {
     const handleToggle = () => setOpened((opened) => !opened);
 
     return (
-        <Menu opened={opened} position='top-end'>
+        <Menu opened={opened} position='top-end' closeOnClickOutside onClose={handleToggle}>
             <Box
                 sx={{
                     paddingTop: theme.spacing.sm,
@@ -64,6 +64,7 @@ export const User: React.FC<UserProps> = ({ username, permission, logo }) => {
             <Menu.Dropdown>
                 <Menu.Item icon={<FontAwesomeIcon icon={faUserPen} />} disabled>Changer son nom</Menu.Item>
                 <Menu.Item icon={<FontAwesomeIcon icon={faUserPen} />} disabled>Changer son mot de passe</Menu.Item>
+                <Menu.Item icon={<FontAwesomeIcon icon={faIdBadge} />} color={theme.colors.teal[6]}>Changer avatar</Menu.Item>
                 <Menu.Item icon={<FontAwesomeIcon icon={faPersonWalkingDashedLineArrowRight} />} color='red'>Se deconnecter</Menu.Item>
             </Menu.Dropdown>
         </Menu>
