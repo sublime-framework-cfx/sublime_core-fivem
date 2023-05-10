5726e5ae28b7c645aef9a90ec0127df455536623
SUP2Ak <cormier.wesley@gmail.com>
2023-05-11 00:54:49 +0200

    refactor(web.mantine): remove theme wip

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -6,6 +6,6 @@ export const themeOverride: MantineThemeOverride = {
   fontFamily: 'Ubuntu',
   colors: {
     // or replace default theme color
-    dark: ['white', 'rgba(31,17,39,0.38)', 'white', 'white', 'white', 'rgba(31,17,39,0.80)', 'rgba(31,17,39,0.80)' , 'rgba(39,54,102,0.80)'],
+    //dark: ['white', 'rgba(31,17,39,0.38)', 'white', 'white', 'white', 'rgba(31,17,39,0.80)', 'rgba(31,17,39,0.80)' , 'rgba(39,54,102,0.80)'],
   },
 };
