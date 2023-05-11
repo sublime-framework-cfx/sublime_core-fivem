c3151cfd4f2cd26566dad20472efd5956e75af01
SUP2Ak <cormier.wesley@gmail.com>
2023-05-11 03:51:42 +0200

    tweak(web/MainProfils): Remove skeleton

--- web/src/features/profils/MainProfils.tsx
+++ web/src/features/profils/MainProfils.tsx
@@ -78,29 +78,24 @@ const MainProfilesMenu: React.FC = () => {
                 <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
                     {/*Call Char components from <NavChar> texture ...*/}
                     <Text>Char component (skin)</Text>
-                    <Skeleton visible={true} width="100%" height="100%"/>
                 </Aside>
             }
             footer={
                 <Footer height={60} p="md">
                     {/*Footer, help, link ...*/}
                     <Text>Link (discord ...) + help</Text>
-                    <Skeleton visible={true} width="100%" height="100%"/>
                 </Footer>
             }
             header={
                 <Header height={{ base: 50, md: 70 }} p="md">
                     {/* Welcome User ... */}
                     <Text>Welcome on sublime core {data.username ||'SUP2Ak'} ...</Text>
-                    <Skeleton visible={true} width="100%" height="100%"/>
                 </Header>
             }
         >
             
             {/*Main compoenent to add char 3d view + stats rect*/}
-            <Skeleton visible={true} width="100%" height="100%"/>
             <Text color="black">Main View 3D char + Stats of Char + Cam button</Text>
-            <Skeleton visible={true} width="100%" height="100%"/>
         </AppShell>
     );
 }
