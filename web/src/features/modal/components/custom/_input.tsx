7c1d71dd57c5d3adb3d3905e9941d1ade0809100
SUP2Ak <cormier.wesley@gmail.com>
2023-05-12 22:08:03 +0200

    feat(web/modal.custom): add checkbox/input component

---
+++ web/src/features/modal/components/custom/_input.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TextInput } from '@mantine/core';
+
+export interface Data {
+    max: number;
+    min: number;
+    required: boolean;
+    placeholder: string;
+    description: string;
+}
+
+interface Props {
+    key: number;
+    index: number;
+    label?: string;
+    data?: Data;
+    onChanged: (index: number, value: string) => void;
+}
+
+export const InputField: React.FC<Props> = ({ index, key, label, data, onChanged }) => {
+    return (
+        <>
+            <TextInput
+                key={key}
+                label={label}
+                placeholder={data?.placeholder || ''}
+                description={data?.description || ''}
+                required={data?.required || false}
+                minLength={data?.min || 0}
+                maxLength={data?.max || 255}
+                onChange={(event) => onChanged(index, event.target.value)}
+            />
+        </>
+    );
+};
---
+++ web/src/features/modal/components/custom/index.ts
@@ -0,0 +1,2 @@
+export * from './_checkbox';
+export * from './_input';
