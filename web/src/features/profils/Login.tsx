c4f14d0af4ac715b1bb9402b9bd5b92d0bf1b91a
SUP2Ak <cormier.wesley@gmail.com>
2023-05-13 22:28:37 +0200

    feat(web/profils): add login modals

--- web/src/features/profils/Login.tsx
+++ web/src/features/profils/Login.tsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Modal,
+  TextInput,
+  PasswordInput,
+  Group,
+  Stack,
+  Text,
+} from '@mantine/core';
+import AnimatedButton from '../modal/components/buttons';
+import { faCheck, faXmark, faLock, faUserShield } from '@fortawesome/free-solid-svg-icons';
+import { useNuiEvent } from '../../hooks/useNuiEvent';
+import { fetchNui } from '../../utils/fetchNui';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [onForgot, setForgot] = useState<boolean>(false);
+
+  const areRequiredFieldsCompleted =
+    username.length << 1 && password.length << 1;
+
+  useNuiEvent('sl:login:opened', () => {
+    setShowModal(true);
+  });
+
+  const handleSubmit = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 200));
+    setShowModal(false);
+    fetchNui('sl:login:submit', { username, password });
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <Modal
+        opened={showModal}
+        centered
+        onClose={() => handleSubmit}
+        title='Login'
+        size='xs'
+        withCloseButton={false}
+        padding='xs'
+      >
+        <Stack>
+          <TextInput
+            label='Username'
+            withAsterisk
+            placeholder='Your username'
+            onChange={(event) => setUsername(event.target.value)}
+            icon={<FontAwesomeIcon icon={faUserShield} size='xs' fade />}
+          />
+          <PasswordInput
+            withAsterisk
+            label='Password'
+            placeholder='Your password'
+            onChange={(event) => setPassword(event.target.value)}
+            icon={<FontAwesomeIcon icon={faLock} size='xs' fade />}
+          />
+          <Text
+            c='dimmed'
+            td='underline'
+            size='sm'
+            {...(onForgot
+              ? {
+                  style: { color: 'white', cursor: 'help' },
+                  onMouseLeave: () => setForgot(false),
+                  onClick: () => console.log('forgot password'),
+                }
+              : {
+                  style: { color: 'gray' },
+                  onMouseEnter: () => setForgot(true),
+                }
+              )
+            }
+          >
+            Mot de passe oublié?
+          </Text>
+        </Stack>
+        <Group position='right' pt={10}>
+          <AnimatedButton
+            iconAwesome={faXmark}
+            text='Annuler'
+            onClick={handleSubmit}
+            color='red'
+            args={false}
+          />
+          <AnimatedButton
+            iconAwesome={faCheck}
+            text='Valider'
+            onClick={handleSubmit}
+            color='green'
+            args={true}
+            isDisabled={!areRequiredFieldsCompleted}
+          />
+        </Group>
+      </Modal>
+    </>
   );
 };
