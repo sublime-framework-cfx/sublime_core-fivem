bd7bdf2fe531ac7a0f473fc2ad1a1d2d927c21e4
SUP2Ak <cormier.wesley@gmail.com>
2023-05-13 22:28:13 +0200

    refactor(server/class.profils): a lot of change

--- server/class/profil.lua
+++ server/class/profil.lua
@@ -3,43 +3,39 @@ sl.profils = {}
 ---@param source integer
 ---@return table
 local function GetAllIdentifiers(self, source) ---@todo move in module
+    local listId = { steam = true, license = true, xbl = true, ip = true, discord = true, live = true }
     self.identifiers = {}
-    for _, v in ipairs(GetPlayerIdentifiers(source)) do
-        if v:match('steam:') then
-            self.identifiers.steam = v:gsub('steam:', '')
+    for _,v in ipairs(GetPlayerIdentifiers(source)) do
+        if listId[v:match('([^:]+)')] then
+            self.identifiers[v:match('([^:]+)')] = v:gsub('([^:]+):', '')
         end
-        if v:match('license:') then
-            self.identifiers.license = v:gsub('license:', '')
-        end
-        if v:match('xbl:') then
-            self.identifiers.xbl = v:gsub('xbl:', '')
-        end
-        if v:match('ip:') then
-            self.identifiers.ip = v:gsub('ip:', '')
-        end
-        if v:match('discord:') then
-            self.identifiers.discord = v:gsub('discord:', '')
-        end
-        if v:match('live:') then
-            self.identifiers.live = v:gsub('live:', '')
-        end
-        self.identifiers.token = GetPlayerToken(source)
     end
+    self.identifiers.token = GetPlayerToken(source)
     return self.identifiers
 end
 
 ---@return boolean|void
-local function GetProfilsDb(self)
-    local query <const> = MySQL.query.await('SELECT * FROM profils WHERE previousId = ?', {self.identifiers.token})
-    if query ~= '[]' then
-        self.id = self.id
-        self.username = query.user
-        self.password = query.password
-        self.stats = json.decode(query.stats)
-        self.permission = query.permission
-        return true
+local function GetProfilsDb(self, from)
+    local query, db = nil, {}
+
+    if from == 'token' then
+        query = MySQL.query.await('SELECT * FROM profils WHERE previousId = ?', {self.identifiers.token})
+    else
+        query = MySQL.query.await('SELECT * FROM profils WHERE user = ? AND password = ?', {self.username, self.password})
     end
-    self:kick('You are not registered in the database? WTF!')
+
+    if query ~= '[]' then
+        for i = 1, #query do
+            local result <const> = query[i]
+            db.id = result.id
+            db.username = result.user
+            db.password = result.password
+            db.permission = result.permission
+            db.stats = json.decode(result.stats)
+            db.createdBy = json.decode(result.createdBy)
+            return db
+        end 
+    end
 end
 
 ---@param reason string
@@ -55,20 +51,37 @@ local function Disconnected(self)
 end
 
 ---@param source integer
+---@param username string
+---@param password string
+---@param external? boolean
 ---@return table
-local function CreateProilsObj(source)
+local function CreateProilsObj(source, username, password, external)
     local self = {}
 
     self.source = source
     self.kick = KickPlayer
     self.identifiers = GetAllIdentifiers(self, source)
+    self.username = username
 
-    if GetProfilsDb(self) then
+    password = password:gsub('%s+', '')
+
+    self.password = joaat(password)
+
+    local db = GetProfilsDb(self, external)
+    if db then
         self.remove = Disconnected
         self.spawned = false
+        self.id = db.id
+        self.username = db.username
+        self.password = db.password
+        self.stats = db.stats
+        self.permission = db.permission
+        self.createdBy = db.createdBy
         sl.profils[source] = self
         sl.previousId[self.identifiers.token] = nil
         return self
+    else 
+        return false
     end
 end
 
