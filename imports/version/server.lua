46e83fd2499efe6778a91e00e89acd4e49df7fe7
SUP2Ak <cormier.wesley@gmail.com>
2023-05-13 17:18:21 +0200

    refactor(import/version): checker auto update for sublime

--- imports/version/server.lua
+++ imports/version/server.lua
@@ -14,7 +14,7 @@ local function Checker(url, webhook, timer, isBeta, perso)
     local message = j.load(('locales/%s'):format(sl.lang), sl.name)
 
     local from <const> = url
-    url = from == 'tebex' and ("https://raw.githubusercontent.com/SUP2Ak/version-tebex-script/main/%s.json"):format(sl.env) or from == 'github' and ('https://api.github.com/repos/SUP2Ak/%s/releases/latest'):format(sl.env) or url
+    url = from == 'github' and ('https://api.github.com/repos/SUBLiME-Association/%s/releases/latest'):format(sl.env) or url
 
     CreateThread(function() 
         Wait(timer or 2500)
@@ -25,8 +25,8 @@ local function Checker(url, webhook, timer, isBeta, perso)
             resp = json.decode(resp)
             -- print(json.encode(resp, {indent=true}))
 
-            if from == 'github' or from == 'tebex' then
-                local lastVersion = from == 'github' and resp.tag_name or from == 'tebex' and resp.version
+            if from == 'github'then
+                local lastVersion = from == 'github' and resp.tag_name
                 lastVersion = lastVersion:gsub('v', '')
 
                 if isBeta then
@@ -38,11 +38,10 @@ local function Checker(url, webhook, timer, isBeta, perso)
     
                 for i = 1, #_version do
                     local current, minimum = tonumber(_version[i]), tonumber(_lastVersion[i])
-    
                     if current ~= minimum then
                         if current < minimum then
                             print('^9---------------------------------------------------------')
-                            print(message.need_update:format(sl.env, version, lastVersion, from == 'github' and resp.html_url or from == 'tebex' and resp.link))
+                            print(message.need_update:format(sl.env, version, lastVersion, from == 'github' and resp.html_url))
                             print('^9---------------------------------------------------------')
                             return
                         else break end
