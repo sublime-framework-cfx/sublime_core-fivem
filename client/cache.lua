a5d485feeb4f8d1b36f74f0f74bf0b7005fb0889
SUP2Ak <cormier.wesley@gmail.com>
2023-05-13 16:10:02 +0200

    refactor(client/cache): SetInterval instead CreateThread

--- client/cache.lua
+++ client/cache.lua
@@ -1,4 +1,4 @@
-local cache = {}
+local cache = _ENV.cache
 
 function cache:set(key, value)
     if not self[key] or self[key] ~= value then
@@ -24,50 +24,46 @@ local GetActiveScreenResolution <const> = GetActiveScreenResolution
 
 local size = 247
 
-CreateThread(function()
-    while true do
-        cache:set('ped', PlayerPedId())
 
-        if size > 250 then
-            local screen_x, screen_y = GetActiveScreenResolution()
-            cache:set('playerid', PlayerId())
-            cache:set('serverid', GetPlayerServerId(cache.playerid))
-            cache:set('screen_x', screen_x)
-            cache:set('screen_y', screen_y)
-            size = 0
-        end
-
-        if sl.game == 'redm' then
-            cache:set('mount', IsPedOnMount(cache.ped) == true and GetMount(cache.ped) or false)
-        end
-
-        local hasWeapon, weaponHash = GetCurrentPedWeapon(cache.ped, true)
-
-        cache:set('weapon', hasWeapon and weaponHash or false)
-
-        local vehicle = GetVehiclePedIsIn(cache.ped, false)
-
-		if vehicle > 0 then
-			cache:set('vehicle', vehicle)
-
-			if not cache.seat or GetPedInVehicleSeat(vehicle, cache.seat) ~= cache.ped then
-				for i = -1, GetVehicleMaxNumberOfPassengers(vehicle) - 1 do
-					if GetPedInVehicleSeat(vehicle, i) == cache.ped then
-						cache:set('seat', i)
-						break
-					end
-				end
-			end
-		else
-			cache:set('vehicle', false)
-			cache:set('seat', false)
-		end
-
-        size += 1
-        Wait(750)
+SetInterval(function()
+    cache:set('ped', PlayerPedId())
+    if size > 250 then
+        local screen_x, screen_y = GetActiveScreenResolution()
+        cache:set('playerid', PlayerId())
+        cache:set('serverid', GetPlayerServerId(cache.playerid))
+        cache:set('screen_x', screen_x)
+        cache:set('screen_y', screen_y)
+        size = 0
     end
-end)
 
+    if sl.game == 'redm' then
+        cache:set('mount', IsPedOnMount(cache.ped) == true and GetMount(cache.ped) or false)
+    end
+
+    local hasWeapon, weaponHash = GetCurrentPedWeapon(cache.ped, true)
+
+    cache:set('weapon', hasWeapon and weaponHash or false)
+
+    local vehicle = GetVehiclePedIsIn(cache.ped, false)
+
+    if vehicle > 0 then
+        cache:set('vehicle', vehicle)
+
+        if not cache.seat or GetPedInVehicleSeat(vehicle, cache.seat) ~= cache.ped then
+            for i = -1, GetVehicleMaxNumberOfPassengers(vehicle) - 1 do
+                if GetPedInVehicleSeat(vehicle, i) == cache.ped then
+                    cache:set('seat', i)
+                    break
+                end
+            end
+        end
+    else
+        cache:set('vehicle', false)
+        cache:set('seat', false)
+    end
+
+    size += 1
+end, 750)
 
 function sl.getCache(key)
     return cache[key]
