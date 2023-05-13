c0901a57c20b3de488132f4cc6f6def2e9484877
SUP2Ak <cormier.wesley@gmail.com>
2023-05-13 15:05:43 +0200

    temp(client/main) add command test modalCustom (remove soon)

--- client/main.lua
+++ client/main.lua
@@ -8,3 +8,27 @@ end)
 sl.onNet('playerLoaded', function(...)
     print('playerLoaded', ...)
 end)
+
+RegisterCommand('tt', function()
+    local tt = sl.openModal({
+        type = 'custom',
+        title = 'Test',
+        options = {
+            { type = 'input', name = 'inputField', label = 'Input Field', required = true },
+            {
+                type = 'checkbox',
+                name = 'checkboxField',
+                label = 'Checkbox Field',
+                checked = true,
+            },
+            {
+                type = 'password',
+                name = 'inputField',
+                label = 'Input Field',
+                required = true,
+            },
+        }
+    })
+    print(tt["1"], tt[1])
+    print(json.encode(tt))
+end)
