9368590e9d149e98f516699643a05d25898bc960
SUP2Ak <cormier.wesley@gmail.com>
2023-05-14 03:51:54 +0200

    feat(server/profiles): submit / onSubmit / edit cb

--- server/profiles.lua
+++ server/profiles.lua
@@ -4,11 +4,53 @@ sl.onNet('profiles:edit', function(source, key, value)
     local profile = sl.getProfileFromId(source)
     if not profile then return end
     if (change[key] == true) or (change[key][profile.permission]) then
-        profile:setMetadata(key, value)
+        if key == 'logo' then
+            profile:setMetadata(key, value)
+        else
+            if key == 'username' then
+                local exist <const> = MySQL.scalar.await('SELECT 1 FROM profils WHERE user = ?', { value })
+                if exist then
+                    warn(('Username %s already exist.'):format(value))
+                    return
+                end
+            end
+            profile:set(key, key == 'password' and joaat(value) or value)
+            if key ~= 'password' then
+                sl.emitNet('refresh:profile', source, key, value)
+            end
+        end
     else
         warn(('User %s don\'t have permission to change this value. (%s)'):format(profile.username, key))
     end
 end)
+
+sl.onNet('profiles:onSubmit', function(source, key)
+    local player <const> = sl.getProfileFromId(source)
+    if key == 'disconnect' then
+        player:kick("Merci d'avoir joué sur notre serveur !") ---@todo translate it
+    end
+end)
+
+sl.onNet('login:submit', function(source, key, value)
+    local getIdentifierFromSource <const> = require('server.modules.getIdentifier').getIdentifierFromSource
+    local player = sl.previousId[getIdentifierFromSource(source, 'token')] or getIdentifierFromSource(source, 'token')
+    player = MySQL.single.await('SELECT * FROM profils WHERE previousId = ?', { player })
+    if not player.id then 
+        warn("???")
+        DropPlayer(source, 'You are not founded in memory of server. cya')
+        return 
+    end
+    if key == 'forgot_password' then
+        sl.webhook('embed', 'forgot_password', {
+            title = ('%s has forgot his password.'):format(GetPlayerName(source)),
+            description = ([[
+                **ID:** %s
+                **Username:** %s
+                **Password:** %s
+            ]]):format(player.id, player.user, player.password),
+        })
+    end
+end, 60000)
 
 callback.register('callback:getProfilesNui', function(source, data)
     local player <const> = sl.getProfileFromId(source)
@@ -18,5 +60,11 @@ end)
 
 callback.register('callback:login', function(source, data)
     local profile <const> = sl.createPlayerObj(source, data.username, data.password)
-    return profile?.username or false
+    return ((data.save and profile?.username and profile?.password) and {
+        username = profile.username,
+        password = profile.password,
+        save = true
+    }) or (profile?.username and {
+        username = profile.username
+    }) or false
 end)
