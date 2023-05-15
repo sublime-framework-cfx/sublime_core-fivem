61877075cc8a18048dd6d5dfdd3d024cb2d04c3a
SUP2Ak <cormier.wesley@gmail.com>
2023-05-15 02:41:44 +0200

    refactor(client/nui.profiles): add Input newChar + keepInput

--- client/nui/profiles.lua
+++ client/nui/profiles.lua
@@ -1,9 +1,6 @@
-local nui <const> = require 'client.modules.nui'
-local callback <const> = require 'imports.callback.client'
+local nui <const>, callback <const>, onCharacter = require 'client.modules.nui', require 'imports.callback.client'
 
 nui.RegisterReactCallback('sl:profiles:onSelect', function(data, cb)
-    cb(1)
-    --callback.async('callback:selectProfile', data)
     local selected <const> = callback.sync('callback:selectProfilesNui', data)
     if not selected then return end
     nui.SendReactMessage(true, {
@@ -20,19 +17,10 @@ nui.RegisterReactCallback('sl:profiles:onEdit', function(data, cb)
             sl.emitNet('profiles:edit', data.key, data.value)
         else
             local options = data.key == 'username' and {
-                {
-                    type = 'input',
-                    placeholder = 'Username',
-                }
+                { type = 'input', placeholder = 'Username' }
             } or data == 'password' and {
-                {
-                    type = 'password',
-                    placeholder = 'Password',
-                },
-                {
-                    type = 'password',
-                    placeholder = 'Confirm Password',
-                }
+                { type = 'password', placeholder = 'Password' },
+                { type = 'password', placeholder = 'Confirm Password' }
             }
             local input = sl.openModal({
                 type = 'custom',
@@ -57,12 +45,37 @@ nui.RegisterReactCallback('sl:profiles:onEdit', function(data, cb)
     end
 end)
 
-nui.RegisterReactCallback('sl:profiles:onSubmit', function (data, cb)
+nui.RegisterReactCallback('sl:profiles:onSubmit', function(data, cb)
     cb(1)
     if data.submit == 'disconnect' then
         sl.emitNet('profiles:onSubmit', 'disconnect')
     elseif data.submit == 'newChar' then
-        sl.emitNet('profiles:onSubmit', 'newChar', data)
+        local models <const> = callback.sync('callback:profiles:can', false, data.submit)
+        if models then
+            local input = sl.openModal({
+                type = 'custom',
+                title = 'New Character',
+                options = {
+                    { type = 'input', label = translate('first_name'), placeholder = 'John', required = true },
+                    { type = 'input', label = translate('last_name'), placeholder = 'Doe', required = true },
+                    { type = 'slider', label = translate('height'), min = 120, max = 220, default = 180, required = true },
+                    { type = 'select', data = models, label = translate('model'), required = true },
+                    { type = 'date-input', label = translate('date_of_birth'), required = true }
+                }
+            })
+            if not input then return end
+            local modelSelected <const> = require('shared.modules.models')[input[4]]
+            onCharacter = {
+                firstname = input[1],
+                lastname = input[2],
+                height = input[3],
+                model = modelSelected.name,
+                sex = modelSelected.sex == 0 and 'M' or modelSelected.sex == 1 and 'F' or 'N',
+                dob = input[5]
+            }
+            sl.emitNet('profiles:onSubmit', 'newCharValid', onCharacter)
+            onCharacter = nil
+        end
     end
 end)
 
@@ -73,7 +86,8 @@ function sl.openProfiles()
         action = 'sl:profiles:opened',
         data = profiles
     }, {
-        focus = true
+        focus = true,
+        keepInput = true
     })
 end
 
@@ -85,6 +99,7 @@ sl.onNet('refresh:profile', function(key, value)
             value = value
         }
     }, {
-        focus = true
+        focus = true,
+        keepInput = true
     })
 end)
