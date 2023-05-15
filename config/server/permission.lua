b3c7175de3c52279aa72bc069027cc4904a812e6
SUP2Ak <cormier.wesley@gmail.com>
2023-05-15 21:48:07 +0200

    feat(config/server.permission): add power

--- config/server/permission.lua
+++ config/server/permission.lua
@@ -1,4 +1,13 @@
-return { -- ps: false no one can change / true everyone can change / table only specific permission can change
+return { -- ps: false no one can change / true everyone can change / table only specific permission can change / number only specific permission can change with power >= number
+
+    power = {
+        ['dev'] = 100,
+        ['owner'] = 100,
+        ['admin'] = 80,
+        ['mod'] = 60,
+        ['helper'] = 20,
+        ['player'] = 0,
+    },
 
     profiles = {
         username = {['owner'] = true, ['admin'] = true}, -- change username
