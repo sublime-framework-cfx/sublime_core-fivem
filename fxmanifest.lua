fc55852d455ad0f5fccc54e190c884d3ec772d44
SUP2Ak <cormier.wesley@gmail.com>
2023-05-16 22:13:24 +0200

    chore(fxmanifest): wip on client / shared again

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,33 +9,23 @@ version '0.0.0'
 repository 'https://github.com/SUBLiME-Association/sublime_core'
 description 'Un framework pour tout type de serveur'
 
-shared_script '_g.lua' -- need to be init first
 server_script 'package/dist/server/server.js' -- need to be init first
+shared_scripts {'_g.lua', 'shared/init.lua'} -- need to be init first
 
-shared_scripts {
+shared_scripts { -- soon removed
     'config/shared/*.lua', -- on top
     'shared/handlers/*.lua',
 }
 
-server_scripts {
-    'config/server/*.lua', -- on top
-    'server/sql/*.lua',
-    'server/class/*.lua',
-    'server/*.lua'
-}
-
-client_scripts {
-    'config/client/*.lua', -- on top
-    'client/*.lua',
-    'client/nui/*.lua'
-}
+server_script 'server/init.lua'
+client_script 'client/init.lua'
 
 ui_page 'web/build/index.html'
 
 files {
     'obj.lua',
     'locales/*',
-    'client/modules/*.lua',
+    'client/modules/**',
     'imports/**/shared.lua',    
     'imports/**/client.lua',
     'web/build/index.html',
