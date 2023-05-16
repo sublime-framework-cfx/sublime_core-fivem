5ad93408ce3f067ca61bd8461fd8fe593ea8338d
SUP2Ak <cormier.wesley@gmail.com>
2023-05-16 22:07:57 +0200

    chore(global): next :eyes:

--- server/modules/unixtime.lua
+++
@@ -1,8 +0,0 @@
----@param unixtime number
----@param format? string-'DD/MM/YYYY'
----@return string as date format
-local function moment(unixtime, format)
-    return exports[sl.name]:convertUnixTimeToDate(unixtime, format)
-end
-
-return moment
--- server/nui.lua
+++
@@ -1,3 +0,0 @@
-function sl.notify(source, select, data)
-    sl.emitNet('notify', source, select, data)
-end
