844dfa3575249201ac3f2d001d8cf0cf7b4d79e2
SUP2Ak <cormier.wesley@gmail.com>
2023-05-18 03:41:26 +0200

    chore(client\modules): rewrite about obj send in func + change dir

---
+++ client/modules/nui/notify.lua
@@ -0,0 +1,13 @@
+local function notify(self, select, data)
+    if not data.position then data.position = 'top-right' end
+
+    if select == 'simple' then
+        self:sendReactMessage(true,{
+            action = 'sl:notification:send',
+            data = data
+        })
+    end
+end
+
+sl.notify = notify -- Export notify function
+sl:onNet('notify', notify) -- Register notify event for server
