d40c38bcb759f0652e3de9ed76d68d97a14b60c8
SUP2Ak <cormier.wesley@gmail.com>
2023-05-20 11:27:22 +0200

    feat(client\modules\loadscreen): init module (need new background xD)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,13 +9,13 @@ version '0.0.0'
 repository 'https://github.com/SUBLiME-Association/sublime_core'
 description 'Un framework pour tout type de serveur'
 
+loadscreen 'client/modules/loadscreen/index.html' -- loadscreen
+loadscreen_manual_shutdown "yes" -- enable manual shutdown
+loadscreen_cursor 'yes' -- enable cursor
+
 server_script 'package/dist/server/server.js' -- need to be init first
 shared_scripts {'_g.lua', 'shared/init.lua'} -- need to be init first
 
-shared_scripts { -- soon removed
-    'config/shared/*.lua', -- on top
-    'shared/handlers/*.lua',
-}
 
 server_script 'server/init.lua'
 client_script 'client/init.lua'
@@ -25,6 +25,8 @@ ui_page 'web/build/index.html'
 files {
     'obj.lua',
     'locales/*',
+    'config/client/*.lua',
+    'config/shared/*.lua',
     'client/modules/**',
     'shared/modules/**',
     'imports/**/shared.lua',    
