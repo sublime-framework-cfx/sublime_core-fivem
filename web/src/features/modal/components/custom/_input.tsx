74fe5a36237ec4e0e6d11faf8bcd3b33bb183357
SUP2Ak <cormier.wesley@gmail.com>
2023-05-28 02:05:57 +0200

    refactor(web/modals): input new wrapper

--- web/src/features/modal/components/custom/_input.tsx
+++ web/src/features/modal/components/custom/_input.tsx
@@ -2,31 +2,32 @@ import React from 'react';
 import { TextInput } from '@mantine/core';
 
 interface Props {
-  key: string;
-  index: number;
+  index: string;
   label?: string;
-  data?: Data;
-  onChanged: (index: number, value: string) => void;
+  data?: any;
+  onChanged: (index: string, value: string, isRequired?: boolean, callback?: boolean) => void;
+  props: any;
 }
 
 export const InputField: React.FC<Props> = ({
   index,
-  key,
   label,
   data,
   onChanged,
+  props
 }) => {
   return (
     <>
       <TextInput
-        key={key}
         label={label}
+        sx={{ paddingTop: '10px' }}
         placeholder={data?.placeholder || ''}
         description={data?.description || ''}
         required={data?.required || false}
         minLength={data?.min || 0}
         maxLength={data?.max || 255}
-        onChange={(event) => onChanged(index, event.target.value)}
+        onChange={(event) => onChanged(index, event.target.value, data?.required || false, data?.callback || false)}
+        error={props.error || false}
       />
     </>
   );
