cb5a729ccd02db009dfcd97e3f59fac220257e36
SUP2Ak <cormier.wesley@gmail.com>
2023-05-28 02:06:45 +0200

    chore(web/modals): Move typing

--- web/src/typings/Modal.ts
+++ web/src/typings/Modal.ts
@@ -1,14 +1,52 @@
 import { ModalsProviderProps } from './config/modals';
 
-export interface ModalProps {
-  type: string; // type of the modal (confirm, input, etc..) !important
-
-  title?: string; // title of the modal used in all types
-
-  // used by : confirm
-  subtitle?: string; // subtitle of the modal
-  description?: string; // description of the modal
-  style?: React.CSSProperties;
-  classes?: ModalsProviderProps;
-  options?: any; // options of the modal
+export interface Option {
+  type: string;
+  label: string;
+  name?: string;
+  checked?: boolean;
+  description?: string;
+  required?: boolean;
+  default?: string | boolean | Date | number | Array<any>;
+  format?: string;
+  icon?: string | string[];
+  placeholder?: string;
+  max?: number;
+  min?: number;
+  step?: number;
+  data?: any;
+  size?: string;
+  error?: string;
+  callback?: boolean;
+  options?: SelectProps;
 }
+
+export interface ModalPropsCustom {
+  title: string;
+  size?: string;
+  options: Option[];
+  useCallback?: boolean;
+}
+
+export interface SelectProps {
+  Array: {label: string, value: string}[];
+}
+
+/*interface Data {
+  max?: number;
+  min?: number;
+  required?: boolean;
+  placeholder?: string;
+  description?: string;
+  label?: string;
+  type?: string;
+  name?: string;
+  value?: string;
+  step?: number;
+  callback?: boolean;
+  options?: SelectProps;
+}
+
+interface SelectProps {
+  Array: {label: string, value: string}[];
+}*/
