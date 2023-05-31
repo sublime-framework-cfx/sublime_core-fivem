404ff74908d5d4a3508aae4d87a82445f8794479
SUP2Ak <cormier.wesley@gmail.com>
2023-05-31 23:54:04 +0200

    feat(clieny\init): richPresence + Statebags (pre-int modules loader)

--- client/init.lua
+++ client/init.lua
@@ -1,6 +1,9 @@
+local modules <const> = require 'config.client.modules'
+
 ---@load modules.handlers
 require 'client.modules.handlers.events'
 require 'client.modules.handlers.nui'
+require 'client.modules.handlers.statebags'
 
 ---@load modules.nui
 require 'client.modules.nui.modals'
@@ -17,3 +20,7 @@ require 'client.modules.main.firstspawn'
 --require 'client.modules.main.main'
 require 'client.modules.main.security'
 --require 'client.modules.loadscreen.main'
+
+if modules.richPresence then
+    if modules.richPresence.discord then require 'client.modules.rich-presence.discord' end
+end
