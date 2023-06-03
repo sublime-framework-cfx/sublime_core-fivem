6333cdb3abd323b0b95d28de51a8f184eacad983
SUP2Ak <cormier.wesley@gmail.com>
2023-06-04 01:57:33 +0200

    feat(config\client.modules): modules manager

--- config/client/modules.lua
+++ config/client/modules.lua
@@ -1,6 +1,21 @@
 return {
-    richPresence = {
-        discord = true
-        -- maybe more in the future
+    ['handlers'] = {
+        'events',
+        'nui',
+        'statebags'
+    },
+    ['nui'] = {
+        'modals',
+        'login',
+        'profiles',
+        'notify'
+    },
+    ['main'] = {
+        'cache',
+        'firstspawn',
+        'security'
+    },
+    ['rich-presence'] = {
+        'discord'
     }
 }
