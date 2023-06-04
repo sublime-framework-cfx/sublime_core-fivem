52d631cf390ea23cf1067d64ffd56f3da789dd3b
SUP2Ak <cormier.wesley@gmail.com>
2023-06-04 14:57:48 +0200

    feat(shared\init): set metatable math
    utils to set method missing and get it from import whitout reload it later

--- shared/init.lua
+++ shared/init.lua
@@ -1,3 +1,14 @@
 ---@load modules.handlers
 require 'shared.modules.handlers.config'
 require 'shared.modules.handlers.translation'
+
+setmetatable(math, {
+    __index = function(self, k)
+        local v = rawget(self, k)
+        if not v then
+            v = require ('imports.math.shared')[k]
+            rawset(self, k, v)
+        end
+        return v
+    end,
+})
