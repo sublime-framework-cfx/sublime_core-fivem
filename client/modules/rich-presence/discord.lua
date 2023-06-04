f793ae05e1e8c79e98edfb8ab9f7fb19cf2dbf09
SUP2Ak <cormier.wesley@gmail.com>
2023-06-04 06:03:54 +0200

    refactor(client\rich-presence.discord): boutton setter

--- client/modules/rich-presence/discord.lua
+++ client/modules/rich-presence/discord.lua
@@ -4,9 +4,11 @@ local SetRichPresence <const> = SetRichPresence
 local GetGameTimer <const> = GetGameTimer
 local richPresence = {}
 
+---@param key string
+---@param value any
 function richPresence:set(key, value)
     if self[key] ~= value then self[key] = value end
-    SetRichPresence(('Players: %s/%s\n[%s] - %s'):format(self.count or GlobalState.playersCount, discord.maxPlayers, cache.serverid, not LocalPlayer.state.username and self.username or LocalPlayer.state.username))
+    SetRichPresence(('Players: %s/%s\n[%s] - %s'):format(self.count--[[@as number]] or GlobalState.playersCount--[[@as number]], discord.maxPlayers, cache.serverid, LocalPlayer.state.name or LocalPlayer.state.username or self.username))
 end
 
 CreateThread(function()
@@ -22,26 +24,27 @@ CreateThread(function()
     SetDiscordRichPresenceAssetSmall(discord.smallIcon)
     SetDiscordRichPresenceAssetSmallText(discord.smallIconText)
 
-    if discord.buttons and #discord.buttons > 0 then
+    if discord?.buttons[0] then
         local SetDiscordRichPresenceAction <const> = SetDiscordRichPresenceAction
-        for i = 0, #(discord.buttons) do
-            local button <const> = discord.buttons[i]
-            SetDiscordRichPresenceAction(i, button.text, button.url)
+        if #(discord.buttons) > 1 then
+            warn('Discord Rich Presence: You can only have 2 buttons')
+        elseif #(discord.buttons) > 0 then
+            for i = 0, #(discord.buttons) do
+                local button <const> = discord.buttons[i]
+                SetDiscordRichPresenceAction(i, button.text, button.url)
+            end
+        else
+            SetDiscordRichPresenceAction(0, discord.buttons[0].text, discord.buttons[0].url)
         end
     end
 
-    local PlayerId <const> = PlayerId
-    local GetPlayerServerId <const> = GetPlayerServerId
 
-    cache.playerid = PlayerId()
-    cache.serverid = GetPlayerServerId(cache.playerid)
-    local playerName = GetPlayerName(cache.playerid)
-    
-    richPresence:set('username', playerName)
+    local playerName <const> = GetPlayerName(cache.playerid) 
+    richPresence:set('username', playerName--[[@as string]]) -- Used for the rich presence to avoid nil value
 end)
 
 AddStateBagChangeHandler('playersCount', 'global', function(bagName, key, value, reserved, replicated)
     local time <const>, timer <const> = GetGameTimer(), 4000
-    while (time + timer) > GetGameTimer() do Wait(1000) end
+    while (time + timer) > GetGameTimer() do Wait(1000) end --- wait 4 seconds before updating the rich presence (to avoid spamming discord api & await playersCount update)
     richPresence:set('count', GlobalState.playersCount == value and value or GlobalState.playersCount)
 end)
