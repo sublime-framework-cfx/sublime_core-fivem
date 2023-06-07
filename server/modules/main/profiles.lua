11ad3ffcb29a9f09454bd6871d4769faa4ef877f
SUP2Ak <cormier.wesley@gmail.com>
2023-06-07 02:17:02 +0200

    feat(server\profiles): callback:login look if playerobj already exist to spawn

--- server/modules/main/profiles.lua
+++ server/modules/main/profiles.lua
@@ -1,14 +1,15 @@
-local change <const> = require('config.server.permission').profiles
+local change <const> = require 'config.server.permission'.profiles
+local mysql <const> = require 'server.modules.main.mysql'
 
 sl:onNet('profiles:edit', function(source, key, value)
-    local profile <const> = sl.getProfileFromId(source)
+    local profile <const> = sl:getProfileFromId(source)
     if not profile then return end
     if profile:hasPermission(change[key]) then
         if key == 'logo' then
             profile:setMetadata(key, value)
         else
             if key == 'username' then
-                local exist <const> = MySQL.scalar.await('SELECT 1 FROM profils WHERE user = ?', { value })
+                local exist <const> = mysql.checkUserExist(value)
                 if exist then
                     warn(('Username %s already exist.'):format(value))
                     return
@@ -25,7 +26,7 @@ sl:onNet('profiles:edit', function(source, key, value)
 end)
 
 sl:onNet('profiles:onSubmit', function(source, key, data)
-    local player <const> = sl.getProfileFromId(source)
+    local player <const> = sl:getProfileFromId(source)
     if key == 'disconnect' then
         player:kick("Merci d'avoir joué sur notre serveur !") ---@todo translate it
     elseif key == 'newCharValid' then
@@ -42,29 +43,31 @@ sl:onNet('profiles:onSubmit', function(source, key, data)
     end
 end)
 
+---@todo need to be rewrite
 sl:onNet('login:submit', function(source, key, value)
     --local getIdentifierFromSource <const> = require('server.modules.getIdentifier').getIdentifierFromSource
-    local player = sl.previousId[sl.getIdentifierFromId(source, 'token')] or sl:getIdentifierFromId(source, 'token')
-    player = MySQL.single.await('SELECT * FROM profils WHERE previousId = ?', { player })
-    if not player.id then 
-        warn("???")
-        DropPlayer(source, 'You are not founded in memory of server. cya')
-        return 
-    end
-    if key == 'forgot_password' then
-        sl:webhook('embed', 'forgot_password', {
-            title = ('%s has forgot his password.'):format(GetPlayerName(source)),
-            description = ([[
-                **ID:** %s
-                **Username:** %s
-                **Password:** %s
-            ]]):format(player.id, player.user, player.password),
-        })
-    end
+    --local player = sl.tempId[sl:getIdentifierFromId(source, 'token')] or sl:getIdentifierFromId(source, 'token') ---@todo change it!!!!
+    --player = MySQL.single.await('SELECT * FROM profils WHERE previousId = ?', { player }) ---@todo change it!!!!
+    --
+    --if not player.id then 
+    --    warn("???")
+    --    DropPlayer(source, 'You are not founded in memory of server. cya')
+    --    return 
+    --end
+    --if key == 'forgot_password' then
+    --    sl:webhook('embed', 'forgot_password', {
+    --        title = ('%s has forgot his password.'):format(GetPlayerName(source)),
+    --        description = ([[
+    --            **ID:** %s
+    --            **Username:** %s
+    --            **Password:** %s
+    --        ]]):format(player.id, player.user, player.password),
+    --    })
+    --end
 end, 60000)
 
 callback.register('callback:profiles:can', function(source, data)
-    local player <const> = sl.getProfileFromId(source)
+    local player <const> = sl:getProfileFromId(source)
     if not player then return false end
     if data == 'newChar' then
         local listModel <const>, models = require 'config.shared.models', {}
@@ -79,22 +82,25 @@ callback.register('callback:profiles:can', function(source, data)
         end)
         for i = 1, #keys do
             local k = keys[i]
-            models[#models + 1] = {
-                label = listModel[k].label,
-                value = listModel[k].name or k,
-            }
+            models[#models + 1] = { label = listModel[k].label, value = listModel[k].name or k }
         end
         return #models > 0 and models or false
     end
+    return false
 end)
 
 callback.register('callback:getProfilesNui', function(source, data, cache)
-    local player <const> = sl.getProfileFromId(source)
+    local player <const> = sl:getProfileFromId(source)
     if not player then return false end
-    return not cache and player:loadNuiProfiles() or player.chars
+    return not cache and player:loadNuiProfiles()
 end)
 
 callback.register('callback:login', function(source, data)
-    local profile <const> = sl:createProfileObj(source, data.username, data.password)
-    return profile?.username and profile.username or false
+    if not data then
+        local player <const> = sl:getProfileFromId(source)
+        return player?.username or false
+    else
+        local profile <const> = sl:createProfileObj(source, data.username, data.password)
+        return profile?.username or false
+    end
 end)
