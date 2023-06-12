6d5ec52798d5df0c31e4edf74a68c3a0e672aeaa
SUP2Ak <cormier.wesley@gmail.com>
2023-06-12 13:18:26 +0200

    tweak(modules\init): spacing

--- modules/init.lua
+++ modules/init.lua
@@ -13,7 +13,7 @@ for i = 1, #folders do
             require(('modules.%s.%s.%s'):format(folder, 'shared', file))
         end
     end
-    
+
     if files[sl.service] then
         local t <const> = files[sl.service]
         for j = 1, #t do
