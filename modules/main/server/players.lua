193434065233e7618c43b277bb582d70aceecc29
SUP2Ak <cormier.wesley@gmail.com>
2023-06-21 21:56:32 +0200

    chore(server\main.players): new version
    - Remove declare
    - SublimePlayer class
    - New syntaxe
    - Check endpoint
    - Loaded from client (or server only on restart)
    - Implement AutoSave function onResourceStart (wip)
    - Préload playerObject if player is connected from deferrals and swap source onPlayerJoining

--- modules/main/server/players.lua
+++ modules/main/server/players.lua
@@ -1,69 +1,41 @@
-require 'modules.main.server.class.profile'
-
+local SublimePlayer <const> = require 'modules.main.server.class.profile' ---@type SublimePlayer
+local GetPlayerEndpoint <const> = GetPlayerEndpoint
 local config <const>, connectingPlayers, mysql <const> = require 'config.server.setting', {}, require 'modules.mysql.server.function'
-sl.tempId, sl.profiles = {}, {}
-GlobalState.playersCount = 0
-
----@param source integer
-local function PlayerLoaded(source)
-    print('sss,', source)
-    sl:emitNet('playerLoaded', source)
-end
-
-function sl:playerLoaded(source)
-    return PlayerLoaded(source)
-end
-
-sl:onNet('playerLoaded', function(source)
-    local _source = source
-    --sl.loadProfil(_source)
-    print('playerLoaded?!', _source)
-    PlayerLoaded(source)
-end, 3000)
+sl.tempId, sl.players, GlobalState.playersCount = {}, {}, 0
 
 ---@param reason string
 AddEventHandler('playerDropped', function(reason) ---@type void
     local _source = source
-    print('playerDropped', _source, reason)
-    sl:playerDropped(_source, reason)
+    local player = sl.getPlayerFromId(_source)
+    if not player then return end
+    player:emitNet('playerDropped')
+    Wait(1000)
+    player:quit()
 end)
-
----@param source integer
----@param reason string
-function sl:playerDropped(source, reason) ---@type void
-    self:emitNet('playerDropped', source, reason)
-end
 
 ---@param tempid number
 AddEventHandler('playerJoining', function(tempId) ---@type void
-    print('playerJoining', source, tempId)
-    local _source = source
+    -- print('playerJoining', source, tempId)
+    local _source, _tempId = source, tonumber(tempId)
     SetTimeout(1000, function()
-        local player <const> = sl:getProfileFromId(tonumber(tempId))
+        local player <const> = sl.getPlayerFromId(_tempId)
+        if not player then return warn(player, _source, 'not player', _tempId) end
         player:set('source', _source)
-        sl.profiles[_source] = player
-        sl.profiles[tonumber(tempId)] = nil
-        for k,v in pairs(sl.profiles[_source]) do
-            print('debug',k, v)
-        end
+        player:set('tempId', _tempId)
+        Wait(200)
+        sl.players[_source] = player
+        sl.players[_tempId] = nil
     end)
 end)
 
 CreateThread(function()
-    local GetPlayerEndpoint <const> = GetPlayerEndpoint
-    local count = 0
     while true do
         Wait(30000) -- 30 seconds
         for tempId in pairs(connectingPlayers) do
+            --print('tempid', tempId, type(tempId), GetPlayerEndpoint(tempId))
             if not GetPlayerEndpoint(tempId) then
                 connectingPlayers[tempId] = nil
             end
-        end
-
-        if next(sl.profiles) then count = #sl.profiles end
-        print('playersCount', count, #sl.profiles)
-        if (count ~= GlobalState.playersCount) then
-            GlobalState.playersCount = count
         end
     end
 end)
@@ -72,11 +44,11 @@ end)
 ---@param setKickReason string
 ---@param deferrals table
 AddEventHandler('playerConnecting', function(name, setKickReason, deferrals) ---@type void
-    local _source, connect <const> = source, sl:getConfig('connect')
-    print('playerConnecting (trying)', _source, GetPlayerEndpoint(_source))
+    local _source, connect <const> = source, sl.getConfig('connect')
+    --print('playerConnecting (trying)', _source, GetPlayerEndpoint(_source))
 
     if connectingPlayers[_source] then
-        print('playerConnecting (already)', _source, GetPlayerEndpoint(_source))
+        --print('playerConnecting (already)', _source, GetPlayerEndpoint(_source))
         return deferrals.done('You are already connecting to the server.')
     end
 
@@ -92,8 +64,87 @@ AddEventHandler('playerConnecting', function(name, setKickReason, deferrals) ---
         local d <const> = require 'modules.deferrals.server.main'
         d(_source, name, setKickReason, deferrals)
     end
-    
 end)
+
+local function StartAutoSave()
+    CreateThread(function()
+        while true do
+            Wait(config.saveInterval)
+            ---@todo: save all about players
+        end
+    end)
+end
+
+---@param from string | 'client' | 'server'
+---@param source integer
+local function PlayerLoaded(from, source)
+    if from == 'client' then
+        sl:emitNet('playerLoaded', source)
+        local player <const> = sl.getPlayerFromId(source)
+        if not player then return end
+        if player.tempId and connectingPlayers[player.tempId] then
+            connectingPlayers[player.tempId] = nil
+        end
+    elseif from == 'server' then
+        local player <const> = sl.getPlayerFromId(source)
+        Wait(500)
+        sl:emit('playerLoaded', source, player or false)
+        if not player then return end
+        if player.tempId and connectingPlayers[player.tempId] then
+            connectingPlayers[player.tempId] = nil
+        end
+    end
+end
+
+---@param resourceName string
+AddEventHandler('onResourceStart', function(resourceName) ---@type void
+    if resourceName ~= GetCurrentResourceName() then return end
+    Wait(1000)
+    local players <const> = GetPlayers()
+    if #players > 0 then
+        for i = 1, #players do
+            local _source = players[i]
+            PlayerLoaded('client', _source)
+        end
+    end
+
+    StartAutoSave()
+end)
+
+---@param resourceName string
+AddEventHandler('onResourceStop', function(resourceName) ---@type void
+    if resourceName ~= sl.name then return end
+    local players <const> = sl.getPlayers()
+    for id, player in pairs(players) do
+        if id then player:quit() end
+    end
+
+    ---@todo: save all characters
+end)
+
+---@param source integer
+sl:onNet('playerLoaded', function(source) ---@type void
+    local _source = source
+    PlayerLoaded('client', _source)
+end)
+
+--[[
+    function sl:saveAllCharacters() --- that will be moved
+        local parameters, size = {}, 0
+        local players <const> = sl.getPlayers(true)
+        if not players or not next(players) then return end
+    
+        for i = 1, #players do
+            local char <const> = players[i].char
+            size += 1
+            parameters[size] = char:prepareSave()
+        end
+    
+        if size > 0 then
+            mysql.updateCharacters(parameters)
+        end
+    end
+]]
 
 ---@todo Not implemented yet 
 --[[
@@ -105,58 +156,20 @@ sl:onNet('setLoadedInstance', function(source)
 end)
 ---]]
 
-function sl:saveAllCharacters()
-    local parameters, size = {}, 0
-    local players <const> = self:getPlayers(true)
-    if not players or not next(players) then return end
+-- RegisterCommand('xp', function(source)
+--     local player = sl.getPlayerFromId(source)
+--     if not player then 
+--         return print('not player xp')
+--     end
+--     print(player:get('test'), 'test', player.source)
+-- end)
 
-    for i = 1, #players do
-        local char <const> = players[i].char
-        size += 1
-        parameters[size] = char:prepareSave()
-    end
-
-    if size > 0 then
-        mysql.updateCharacters(parameters)
-    end
-end
-
-CreateThread(function()
-    while not next(sl.profiles) do Wait(1000) end
-
-    while true do
-        Wait(config.saveInterval)
-        -- Wait(5000) -- 5 seconds
-        sl:saveAllCharacters()
-
-        -- Wait((1000 * 60) * 5) -- 5 minutes
-        -- sl:saveAllVehicles(sql) -- @todo for persistance not implemented yet
-    end
-end)
-
-AddEventHandler('onResourceStart', function(resourceName) ---@type void
-    if resourceName ~= GetCurrentResourceName() then return end
-    Wait(1000)
-    local players <const> = GetPlayers()
-    if #players > 0 then
-        for i = 1, #players do
-            local _source = players[i]
-            sl:playerLoaded(_source)
-        end
-    end
-end)
-
-AddEventHandler('onResourceStop', function(resourceName) ---@type void
-    if resourceName ~= sl.name then return end
-    local profiles <const> = sl:getPlayers()
-    if next(profiles) then
-        for _,v in pairs(profiles) do --- force save profiles
-            v:save()
-        end
-
-        sl:saveAllCharacters() --- force save characters
-    end
-end)
-
-declare(sl.playerLoaded)
-declare(sl.playerDropped)
+-----@param cb fun(source: integer, player: table, reason: string)
+--function sl:playerDropped(cb) ---@type void
+--    print('is init from', self.env)
+--    return self:on('playerDropped', function(source, player, reason)
+--        print('trololol')
+--        print(player, source, 'playerDropped')
+--        cb(source, player, reason)
+--    end)
+--end
