6fe3f3f6df3ddaca93bc8bb6719be0a92efcddf8
SUP2Ak <cormier.wesley@gmail.com>
2023-07-01 20:37:56 +0200

    feat(modules\main): init death server-side

--- modules/main/index.lua
+++ modules/main/index.lua
@@ -3,10 +3,12 @@ return {
         'players',
         'profiles',
         'spawn',
+        'death'
     },
 
     client = {
         'cache',
         'firstspawn',
+        -- 'death' not needed because i loaded it in firstspawn when onCharacterSpawn is played but need to confirm this module from config
     }
 }
