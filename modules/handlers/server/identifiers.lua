6fe75e64d595064cabf0faaa790c7b198a7d64e7
SUP2Ak <cormier.wesley@gmail.com>
2023-07-04 20:13:07 +0200

    tweak(mod\identifiers): mainly adjusting

--- modules/handlers/server/identifiers.lua
+++ modules/handlers/server/identifiers.lua
@@ -1,21 +1,21 @@
 local GetPlayerIdentifiers <const>, GetPlayerIdentifierByType<const>, GetPlayerToken <const> = GetPlayerIdentifiers, GetPlayerIdentifierByType, GetPlayerToken
-local identifiersApproved<const> = { steam = true, license = true, xbl = true, ip = true, discord = true, live = true }
+local identifiersApproved <const> = { steam = true, license = true, xbl = true, ip = true, discord = true, live = true }
 
 ---@param source integer
 ---@param key? string
 ---@return unknown
-local function GetIdentifierFromSource(source, key) ---@todo move in module
+local function GetIdentifierFromSource(source, key) ---@todo move or add in import
     if key == "token" or not identifiersApproved[key] then
         return GetPlayerToken(source)
     end
     local identifier = GetPlayerIdentifierByType(source, key)
-    return (identifier and identifier:gsub('([^:]+):', '') or nil) 
+    return (identifier and identifier:gsub('([^:]+):', '')) or nil
 end
 
 ---@param source integer
 ---@param encode? boolean
 ---@return table|string<json>
-local function GetIdentifiersFromSource(source, encode) ---@todo
+local function GetIdentifiersFromSource(source, encode) ---@todo move or add in import
     local identifiers = {}
     for _,v in ipairs(GetPlayerIdentifiers(source)) do
         local keyIdentifier = v:match('([^:]+)')
@@ -24,7 +24,7 @@ local function GetIdentifiersFromSource(source, encode) ---@todo
         end
     end
     identifiers.token = GetPlayerToken(source)
-    return encode and json.encode(identifiers) or identifiers
+    return (encode and json.encode(identifiers, { indent = true })) or identifiers
 end
 
 sl.getIdentifiersFromId = GetIdentifiersFromSource
