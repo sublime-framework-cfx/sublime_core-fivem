00b3a02e169b60a2967b4075bae4388acba752e2
SUP2Ak <cormier.wesley@gmail.com>
2023-07-19 13:18:22 +0200

    feat(imports\string): add generate UUID

--- imports/string/shared.lua
+++ imports/string/shared.lua
@@ -5,6 +5,15 @@ local function FirstToUpper(str)
     return (str:gsub("^%l", string.upper))
 end
 
+local function GenerateUUID()
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return template:gsub('[xy]', function(repl)
+        local v = (repl == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
+        return ('%x'):format(v)
+    end)
+end
+
 return {
-    firstToUpper = FirstToUpper
+    firstToUpper = FirstToUpper,
+    uuid = GenerateUUID
 }
