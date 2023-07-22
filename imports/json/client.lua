a4d765c5cdae5faa277042e5ec736862d98b9b1a
SUP2Ak <cormier.wesley@gmail.com>
2023-07-22 14:48:23 +0200

    refactor(imports\json): auto-correct path use "." and ".json"

--- imports/json/client.lua
+++ imports/json/client.lua
@@ -5,11 +5,11 @@ local LoadResourceFile <const> = LoadResourceFile
 ---@param resourceName? string
 ---@return string
 local function Loadjson(filePath, resourceName)
-    local resource <const> = resourceName or sl.env or GetCurrentResourceName()
-    local filename <const> = filePath
-    local str <const> = json.decode(LoadResourceFile(resource, ("%s.json"):format(filename)))
+    local resource <const> = resourceName or sl.env or sl.name
+    local path <const> = filePath:gsub('%.json$', ''):gsub('%.', '/')
+    local str <const> = json.decode(LoadResourceFile(resource, ("%s.json"):format(path)))
     if not str then
-        error(('[ERROR] : Le fichier (%s) dans la ressource => %s, n\'a pas pu être chargé'):format(filename, resource), 2)
+        error(('[ERROR] : Le fichier (%s) dans la ressource => %s, n\'a pas pu être chargé'):format(path, resource), 2)
     end
     return str
 end
