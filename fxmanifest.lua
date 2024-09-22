3db4ea8b55f5c80cfd8e921ced9b565bcb74fd99
SUP2Ak <cormier.wesley@gmail.com>
2024-09-22 04:51:45 +0200

    refactor(./): fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,9 @@
+---@todo [NEW] work in progress 0.0.2 to 0.1.0
+
+-- loadscreen 'modules/loadscreen/client/index.html' -- loadscreen
+-- loadscreen_manual_shutdown "yes" -- enable manual shutdown
+-- loadscreen_cursor 'yes' -- enable cursor
+
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
@@ -5,12 +11,20 @@ use_experimental_fxv2_oal 'yes'
 
 name 'sublime_core (fivem)'
 author 'SUBLiME'
-version '0.1.0'
+version '0.1.0 (wip)'
 repository 'https://github.com/sublime-framework-cfx/sublime_core-fivem'
 description 'A new modular framework for FiveM'
 
----@todo New!!! Work in progress 0.0.2 to 0.1.0
+files {
+    'config/modules.lua',
+    'imports/**/shared.lua',
+    'imports/**/client.lua',
+    'import.lua',
+    -- 'modules/**/shared/**',
+    'modules/**/client/**',
+}
 
--- loadscreen 'modules/loadscreen/client/index.html' -- loadscreen
---loadscreen_manual_shutdown "yes" -- enable manual shutdown
---loadscreen_cursor 'yes' -- enable cursor
+shared_scripts {
+    'init.lua',
+    'modules/init.lua',
+}
